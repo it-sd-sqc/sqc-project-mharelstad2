afc51d3cc702e0cb8d9b256e22f80457d9a0d3d8
mharelstad2 <143424191+mharelstad2@users.noreply.github.com>
2023-09-11 20:34:56 -0500

    Update site.js

--- public/site.js
+++ public/site.js
@@ -1,11 +1,29 @@
-  function myFunction() {
-   var element = document.getElementsByTagName('body')[0];   
-   element.classList.toggle("dark-mode");
-   document.getElementById("button").innerHTML="☀️";
+// theme-toggle.js
 
-   if (element.classList.contains("dark-mode")) {
-    document.getElementById("button").innerHTML="☀️";
-   } else 
-    document.getElementById("button").innerHTML="🌙";
-   
+// Function to toggle between light and dark mode
+function toggleTheme() {
+  const body = document.body;
+
+  // Check if dark mode is enabled
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  } else {
+    body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
   }
+}
+
+// Check user's preferred theme from localStorage
+const userPrefersDark = localStorage.getItem("theme") === "dark";
+
+// Apply user's preferred theme
+if (userPrefersDark) {
+  document.body.classList.add("dark");
+}
+
+// Event listener for the theme toggle button
+const themeToggleBtn = document.getElementById("theme-toggle-btn");
+if (themeToggleBtn) {
+  themeToggleBtn.addEventListener("click", toggleTheme);
+}
