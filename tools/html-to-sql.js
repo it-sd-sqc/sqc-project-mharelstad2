1627615e258f02788bb34dd7462d5829a4e3fe0f
Marcus Harelstad <mharelstad2@student.cvtc.edu>
2023-10-15 23:12:04 -0500

    Standard fixes

--- tools/html-to-sql.js
+++ tools/html-to-sql.js
@@ -1,34 +1,34 @@
-const fs = require('fs');
+const fs = require('fs')
 
 // Read the HTML file (change 'input.html' to your HTML file's path)
-const inputFilePath = 'input.html';
-const htmlData = fs.readFileSync(inputFilePath, 'utf8');
+const inputFilePath = 'input.html'
+const htmlData = fs.readFileSync(inputFilePath, 'utf8')
 
 // Convert the HTML data to SQL statements (modify this logic as needed)
-const sqlStatements = `INSERT INTO your_table_name (html_data_column) VALUES ('${htmlData.replace(/'/g, "''")}');`;
+const sqlStatements = `INSERT INTO your_table_name (html_data_column) VALUES ('${htmlData.replace(/'/g, "''")}');`
 
 // Write the SQL statements to an output file (change 'output.sql' to your desired output file's path)
-const outputFilePath = 'output.sql';
-fs.writeFileSync(outputFilePath, sqlStatements);
+const outputFilePath = 'output.sql'
+fs.writeFileSync(outputFilePath, sqlStatements)
 
-console.log(`HTML data has been converted to SQL and saved to ${outputFilePath}`);
+console.log(`HTML data has been converted to SQL and saved to ${outputFilePath}`)
 
 // Correct the file path format
-const lukesFilePath = 'C:/Users/Owner/Documents/CVTC/SQC/sqc-project-mharelstad2/data';
+const lukesFilePath = 'C:/Users/Owner/Documents/CVTC/SQC/sqc-project-mharelstad2/data'
 
 // Read the input file
 try {
-    const content = fs.readFileSync(lukesFilePath, 'utf8');
-    
-    // Process the content as needed
-    console.log('Content from the input file:');
-    console.log(content);
+  const content = fs.readFileSync(lukesFilePath, 'utf8')
+
+  // Process the content as needed
+  console.log('Content from the input file:')
+  console.log(content)
 } catch (error) {
-    if (error.code === 'ENOENT') {
-        console.error(`File not found at path: ${lukesFilePath}`);
-    } else {
-        console.error(`An error occurred: ${error.message}`);
-    }
+  if (error.code === 'ENOENT') {
+    console.error(`File not found at path: ${lukesFilePath}`)
+  } else {
+    console.error(`An error occurred: ${error.message}`)
+  }
 }
 
 const sqlHeader = `
@@ -55,18 +55,18 @@ VALUES (value1, value2),
 
 INSERT INTO your_table_name2 (column1_name, column2_name)
 VALUES (value5, value6),
-       (value7, value8);`;
+       (value7, value8);`
 
 // Define the output file path
-const sqlFilePath = './docs/generated-schema.sql';
+const sqlFilePath = './docs/generated-schema.sql'
 
 // Ensure the directory exists or create it
-const outputDir = sqlFilePath.substring(0, sqlFilePath.lastIndexOf('/'));
+const outputDir = sqlFilePath.substring(0, sqlFilePath.lastIndexOf('/'))
 if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir, { recursive: true });
+  fs.mkdirSync(outputDir, { recursive: true })
 }
 
 // Write the SQL script content to the output file
-fs.writeFileSync(sqlFilePath, sqlHeader);
+fs.writeFileSync(sqlFilePath, sqlHeader)
 
-console.log(`SQL script has been written to ${sqlFilePath}`);
+console.log(`SQL script has been written to ${sqlFilePath}`)
